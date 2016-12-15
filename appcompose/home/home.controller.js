abd81e111f74f6516360ebb127edd5b2551d1430
Mohammad Shabaz Moosa <momoosa@microsoft.com>
2016-12-15 17:54:48 +0530

    Setup tabs

--- appcompose/home/home.controller.js
+++ appcompose/home/home.controller.js
@@ -9,16 +9,37 @@
    */
   function homeController(dataService){
     var vm = this;  // jshint ignore:line
-    vm.title = 'my new home controller changed';
+    vm.title = 'Home';
+    vm.tabs = [
+      {title: 'Unified', icon: 'Brightness', content: 'unified'},
+      {title: 'Canned', icon: 'Mail', content: 'canned'},
+      {title: 'Enoji', icon: 'Emoji', content: 'emoji'},
+      {title: 'Quotes', icon: 'Message', content: 'quote'},
+      {title: 'TLDR', icon: 'PreviewLink', content: 'tldr'},
+    ];
     vm.dataObject = {};
 
-    getDataFromService();
+    init();
 
     function getDataFromService(){
       dataService.getData()
         .then(function(response){
           vm.dataObject = response;
         });
+    }
+
+    function init() {
+      initComponents();
+      getDataFromService();
+    }
+
+    function initComponents() {
+      setTimeout(function() {
+        var PivotElements = document.querySelectorAll(".ms-Pivot");
+        for(var i = 0; i < PivotElements.length; i++) {
+          new fabric['Pivot'](PivotElements[i]);
+        }
+      }, 1000);
     }
   }
 
