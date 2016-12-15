b5ca389eb8596735857c8ff3adb8882209499f17
Mohammad Shabaz Moosa <momoosa@microsoft.com>
2016-12-15 15:29:06 +0530

    Initial commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="angularjs/angular-sanitize.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="office-js/office-js.d.ts" />
